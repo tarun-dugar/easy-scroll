2fcbd16445d4df49236e8fbdfadbe67cff556cd3
Tarun Dugar <tarundugar1992@gmail.com>
2017-12-22 22:17:56 +0530

    multiple builds for different env

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-module.exports = {
+module.exports = [{
   'entry': './src/index.js',
   'module': {
     'loaders': [{
@@ -14,7 +14,39 @@ module.exports = {
     'path': path.resolve(__dirname, './dist'),
     'publicPath': '/',
     'filename': 'easy-scroll.js',
-    'library': 'webpackNumbers',
+    'library': 'easyScroll',
     'libraryTarget': 'commonjs2'
   }
-}
+}, {
+  'entry': './src/index.js',
+  'module': {
+    'loaders': [{
+      'test': /\.js$/,
+      'exclude': /node_modules/,
+      'loader': 'babel-loader'
+    }]
+  },
+  'output': {
+    'path': path.resolve(__dirname, './dist'),
+    'publicPath': '/',
+    'filename': 'easy-scroll.window.js',
+    'library': 'easyScroll',
+    'libraryTarget': 'window'
+  }
+}, {
+  'entry': './src/index.js',
+  'module': {
+    'loaders': [{
+      'test': /\.js$/,
+      'exclude': /node_modules/,
+      'loader': 'babel-loader'
+    }]
+  },
+  'output': {
+    'path': path.resolve(__dirname, './dist'),
+    'publicPath': '/',
+    'filename': 'easy-scroll.var.js',
+    'library': 'easyScroll',
+    'libraryTarget': 'var'
+  }
+}]
