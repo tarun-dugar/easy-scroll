94987db6f1b950be234cf066f1526742c23cc72d
Tarun Dugar <tarundugar1992@gmail.com>
2017-12-22 22:20:31 +0530

    umd bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-module.exports = [{
+module.exports = {
   'entry': './src/index.js',
   'module': {
     'loaders': [{
@@ -15,38 +15,6 @@ module.exports = [{
     'publicPath': '/',
     'filename': 'easy-scroll.js',
     'library': 'easyScroll',
-    'libraryTarget': 'commonjs2'
+    'libraryTarget': 'umd'
   }
-}, {
-  'entry': './src/index.js',
-  'module': {
-    'loaders': [{
-      'test': /\.js$/,
-      'exclude': /node_modules/,
-      'loader': 'babel-loader'
-    }]
-  },
-  'output': {
-    'path': path.resolve(__dirname, './dist'),
-    'publicPath': '/',
-    'filename': 'easy-scroll.window.js',
-    'library': 'easyScroll',
-    'libraryTarget': 'window'
-  }
-}, {
-  'entry': './src/index.js',
-  'module': {
-    'loaders': [{
-      'test': /\.js$/,
-      'exclude': /node_modules/,
-      'loader': 'babel-loader'
-    }]
-  },
-  'output': {
-    'path': path.resolve(__dirname, './dist'),
-    'publicPath': '/',
-    'filename': 'easy-scroll.var.js',
-    'library': 'easyScroll',
-    'libraryTarget': 'var'
-  }
-}]
+}
